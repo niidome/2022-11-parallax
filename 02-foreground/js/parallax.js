2eb828c8719adbcb926bfc99bb1738c19f4e7e54
NIIDOME Hiromasa <niidome@urb.co.jp>
2022-11-27 03:50:17 +0900

    古いパソコンのカクツキ防止 スクロールイベント時の処理を描画に合わせて適宜スキップ translate3d を使い GPU に処理をさせる

--- 02-foreground/js/parallax.js
+++ 02-foreground/js/parallax.js
@@ -1,11 +1,26 @@
 /* 背景位置をずらす要素を取得 */
 let background = document.getElementsByClassName('parallax-background')[0];
 
+/* 処理が実行中かどうか */
+let isActive = false;
+
 /* スクロールイベントが発生する時に処理を呼び出す */
 window.addEventListener('scroll', function (e) {
-    /* 現在のスクロール位置を取得、動きをズラすために少し小さな値に調整(0.8倍) */
-    let scroll = window.pageYOffset * 0.8;
+    if (!isActive) {
+        /* 処理実行中 */
+        isActive = true;
 
-    /* CSS の background-position を更新、高さ方向を top から下に移動させる */
-    background.style.backgroundPosition = 'top ' + scroll + 'px center';
+        /* 画面の描画のタイミングで呼び出し */
+        window.requestAnimationFrame(function () {
+            /* 現在のスクロール位置を取得、動きをズラすために少し小さな値に調整(0.8倍) */
+            let scroll = parseInt(window.pageYOffset * 0.8);
+
+            /* CSS の background-position を更新、高さ方向を top から下に移動させる */
+            //            background.style.backgroundPosition = 'top ' + scroll + 'px center';
+            background.style.transform = 'translate3d(0, ' + scroll + 'px, 0)';
+
+            /* 処理終了 */
+            isActive = false;
+        });
+    }
 });
